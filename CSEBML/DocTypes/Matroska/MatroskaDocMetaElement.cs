58609a9617a8faede727467d2e4093c98aacf749
DvdKhl <DvdKhl@web.de>
2013-03-27 14:32:54 +0100

    Updated MatroskaDocType to include v4 elements

--- CSEBML/DocTypes/Matroska/MatroskaDocMetaElement.cs
+++ CSEBML/DocTypes/Matroska/MatroskaDocMetaElement.cs
@@ -16,8 +16,8 @@ namespace CSEBML.DocTypes.Matroska {
 
 		public MatroskaDocMetaElement(Int32 id, string options, Object defaultValue, Predicate<Object> rangeCheck, Int32[] parentIds, string description) {
 			Id = id;
-			IsMandatory = options[4] == 'M' && options[5] == 'a';
-			Multiple = options[6] == 'M' && options[7] == 'u';
+			IsMandatory = options[5] == 'M' && options[6] == 'a';
+			Multiple = options[7] == 'M' && options[8] == 'u';
 			DefaultValue = defaultValue;
 			ParentIds = parentIds;
 			RangeCheck = rangeCheck;
@@ -27,7 +27,8 @@ namespace CSEBML.DocTypes.Matroska {
 				(options[0] == '1' ? MatroskaVersion.V1 : 0) |
 				(options[1] == '2' ? MatroskaVersion.V2 : 0) |
 				(options[2] == '3' ? MatroskaVersion.V3 : 0) |
-				(options[3] == 'W' ? MatroskaVersion.WebM : 0);
+				(options[3] == '4' ? MatroskaVersion.V4 : 0) |
+				(options[4] == 'W' ? MatroskaVersion.WebM : 0);
 		}
 
 		internal bool CompatibleTo(MatroskaVersion version) {
